268d680a842a3cffd047d9e2d29c2664f3229a19
Jun Yeop Kim <84053682+jkim3663@users.noreply.github.com>
2025-07-30 22:02:13 -0400

    initial data

---
+++ database/data/user-data.sql
@@ -0,0 +1,5 @@
+INSERT INTO USER (NAME, EMAIL, PW_HASH)
+VALUES ('Alice Johnson', 'alice@example.com', '$2b$10$w3aP0yH...yX4h9qPiXuA6');
+
+INSERT INTO USER (NAME, EMAIL, PW_HASH)
+VALUES ('Bob Smith', 'bob@example.com', '$2b$10$uVvE2W...7sCqTdrX7gqD.');
